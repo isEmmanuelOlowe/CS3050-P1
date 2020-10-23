43d3ca2aacd97a0e21751388523524527f589134
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2020-10-23 14:56:23 +0100

    No test Completion of Ex1

--- ex1.pl
+++ ex1.pl
@@ -1,13 +1,29 @@
-isFaculty(X, Y).
-isDepartment(X, Y).
-
 % 1
-collaborate(X, Y):- moduleBy(X, Y), module(X, Z).
+moduleBy(cs3050, logicDepartment).
+moduleBy(cs3050, reasoningDepartment).
+moduleBy(cs3050, prologDepartment).
+moduleBy(cs3099, machineLearningDepartment).
+moduleBy(mt2501, linearDepartment).
+moduleBy(mt2501, appliedDepartment).
 % 2
-enrolled(X, Y);
+enrolled(scott, cs3050).
+enrolled(scott, cs3099).
+enrolled(scott, mt3501).
+enrolled(jemma, cs3050).
+enrolled(jackson, mt3508).
 % 3
-commonModule(X, Y) :- enrolled(X, Z), enrolled(Y, Z).
+commonModule(X, Y):- enrolled(X, Z), enrolled(Y, Z).
 % 4
-moduleBy()
+school(logicDepartment, computerScience).
+school(reasoningDepartment, computerScience).
+school(prologDepartment, computerScience).
+school(machineLearningDepartment, computerScience).
+school(linearDepartment, mathematics).
+school(appliedDepartment, mathematics).
+schoolModule(X, Y):- moduleBy(X, Z), school(Z, Y).
 % 5
-belongs(X, Y) :-
+faculty(computerScience, science).
+faculty(mathematics, science).
+belongsTo(X, Y) :- moduleBy(X, Y); school(X, Y); faculty(X, Y); moduleBy(X, Z), school(Z, Y); school(X, Z), faculty(Z, Y); moduleBy(X, Z), school(Z, Q), faculty(Q, Y).
+% 6
+studentOf(X, Y) :- enrolled(X, Y); enrolled(X, Z), belongsTo(Z, Y).
